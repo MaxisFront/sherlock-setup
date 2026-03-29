9344ab8a99816cbcb44ffb9d43ad56c2438f6fd9
MaxisFront <240985446+MaxisFront@users.noreply.github.com>
2026-03-28 18:20:38 -0600

    docs: fix typos in the sherlock-setup file

--- sherlock-setup.sh
+++ sherlock-setup.sh
@@ -8,7 +8,7 @@
 # Life script.
 #
 # GitHub: https://github.com/MaxisFront/sherlock-setup
-# Use: sherlock-setup.sh <MachineName> <URL> [password]
+# Use: sherlock-setup <MachineName> <URL> [password]
 # ===============================================================================
 
 sherlock-setup() {
@@ -16,14 +16,14 @@ sherlock-setup() {
   # Verify dependencies
   for cmd in 7z wget sha256sum; do 
     if ! command -v "$cmd" &>/dev/null; then
-      echo -e "\e[31m[!] The command $cmd doesn't exist"
+      echo -e "\e[31m[!] The command $cmd doesn't exist\e[0m"
       return 1
     fi
   done
 
   if [[ -z  $1 || -z $2 ]]; then
      echo -e "\e[31m[!] URL or machine name not provided\e[0m"
-    echo -e "Use: sherlock-setup.sh <\e[33mmachineName\e[0m> <\e[34mhttps://example.com/filename\e[0m> [password]"
+    echo -e "Use: sherlock-setup <\e[33mmachineName\e[0m> <\e[34mhttps://example.com/filename\e[0m> [password]"
 
     return 1
   fi
@@ -43,7 +43,7 @@ sherlock-setup() {
     # Decompress the .zip file (No output from the '7z' command)
     if 7z x -bso0 -y -p"$password" "$filepath" -o"$name/artifacts/"; then
       
-      echo -e "\e[32m[+]${filename} downloaded and extracted successfully\e[0m"
+      echo -e "\e[32m[+] ${filename} downloaded and extracted successfully\e[0m"
       sha256sum "$filepath" > "$name/artifacts/checksum.txt"
 
       mv "$filepath" "$name/artifacts/checksum.txt" "$name/src/"
@@ -52,7 +52,7 @@ sherlock-setup() {
       if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
         cd "$name" || return 1
       else 
-        echo -e "\e[32m[+] Enviroment set up at $(pwd)/$name\e[0m"
+        echo -e "\e[32m[+] Environment set up at $(pwd)/$name\e[0m"
       fi
 
     else
